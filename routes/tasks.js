bdc31cb209be559c1a4b24363f1899d06d9602c4
Juan Rincon <juancra19@gmail.com>
2021-03-16 20:01:55 -0400

    get task by completed added

--- routes/tasks.js
+++ routes/tasks.js
@@ -5,6 +5,7 @@ const router = Router();
 const { getTasks, createTask, updateTask, deleteTask } = require('../controllers/tasks');
 
 router.get('/:id', getTasks);
+router.get('/completed/:id', getTasksByCompleted);
 router.post('/', createTask);
 router.put('/:id', updateTask);
 router.delete('/:id', deleteTask);
